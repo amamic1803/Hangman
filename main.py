354c5dc3c852d51e4aaddec406c0cac117d80f3c
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-07-22 16:12:04 +0200

    development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.messagebox import showwarning
+from tkinter.messagebox import askyesno
 import os
 import sys
 import random
@@ -49,12 +49,13 @@ def change_thickness(event, widget, typ):
 	else:
 		widget.config(highlightthickness=5)
 
-def start_game():
-	global selected_language, en_words, hr_words, letter_coords, word
+def start_game(event=None):
+	global selected_language, en_words, hr_words, letter_coords, word, word_letters
 	word_canvas.delete("all")
 	letter_coords = []
 	if selected_language == "en":
 		word = random.choice(en_words)
+		word_letters = [x for x in word]
 		len_word = len(word)
 		start_coord = (500 - (len_word * 30 + (len_word - 1) * 11)) // 2
 		line_or_space = True
@@ -70,6 +71,32 @@ def start_game():
 	else:
 		word = random.choice(hr_words)
 
+		word_letters = []
+		word_temp = word
+		while len(word_temp) > 0:
+			if len(word_temp) == 1:
+				word_letters.append(word_temp)
+				word_temp = ""
+			elif word_temp[0] + word_temp[1] in ["dž", "nj", "lj"]:
+				word_letters.append(word_temp[0] + word_temp[1])
+				word_temp = word_temp[2:]
+			else:
+				word_letters.append(word_temp[0])
+				word_temp = word_temp[1:]
+
+		len_word = len(word_letters)
+		start_coord = (500 - (len_word * 30 + (len_word - 1) * 11)) // 2
+		line_or_space = True
+		for i in range(2 * len_word - 1):
+			if line_or_space:
+				word_canvas.create_line(start_coord, 40, start_coord + 30, 40, fill="black", width=4)
+				letter_coords.append(start_coord + 14)
+				line_or_space = False
+				start_coord += 30
+			else:
+				start_coord += 11
+				line_or_space = True
+
 def validate_input(full_text):
 	global word
 	if full_text == "":
@@ -80,13 +107,22 @@ def validate_input(full_text):
 		return False
 
 def guess_click(event):
-	pass
+	global word, word_letters
+	inpt = guess_ent.get()
+	if len(inpt) != 0:
+		if inpt in word_letters or inpt == word:
+			guessed_letters.append(inpt)
+		else:
+			missed_letters.append(inpt)
 
 
 if __name__ == '__main__':
 	selected_language = "en"
 	letter_coords = []
 	word = ""
+	word_letters = []
+	guessed_letters = []
+	missed_letters = []
 
 	with open("data/en_words.txt", "r", encoding="utf-8") as file1, open("data/hr_words.txt", "r", encoding="utf-8") as file2:
 		en_words = [i.rstrip("\n") for i in file1.readlines()]
@@ -121,90 +157,26 @@ if __name__ == '__main__':
 	restart_image = PhotoImage(file="images/hangman-restart_image.png")
 	restart_lbl = Label(root, image=restart_image, justify=CENTER, borderwidth=0, background="#fffae6", activebackground="#fffae6", highlightthickness=0)
 	restart_lbl.place(x=0, y=0, width=40, height=40)
+	restart_lbl.bind("<ButtonRelease-1>", start_game)
 
 	word_canvas = Canvas(root, borderwidth=0, highlightthickness=0, background="#fffae6")
 	word_canvas.place(x=0, y=100, width=500, height=50)
 
-	start_game()
+	# word_canvas.create_text(64, 40, text="DŽ", font=("Helvetica", 21, "bold"), fill="black", activefill="black", anchor="s")
 
-	word_canvas.create_text(64, 40, text="DŽ", font=("Helvetica", 21, "bold"), fill="black", activefill="black", anchor="s")
-
-	drawing_lbl = Label(root, image=hangman_images[8], justify=CENTER, borderwidth=0, background="green", activebackground="#fffae6", highlightthickness=0)
+	drawing_lbl = Label(root, image="", justify=CENTER, borderwidth=0, background="green", activebackground="#fffae6", highlightthickness=0)
 	drawing_lbl.place(x=0, y=175, width=200, height=250)
 
 	reg = root.register(validate_input)
 	guess_ent = Entry(root, justify=CENTER, validate="key", validatecommand=(reg, "%P"), background="#ffffff", foreground="#000000", highlightthickness=3, highlightcolor="black", highlightbackground="black", borderwidth=0, font=("Helvetica", 15))
-	guess_ent.place(x=250, y=200, width=173, height=35)
+	guess_ent.place(x=250, y=195, width=173, height=35)
 
 	guess_btn = Label(root, text="Try", font=("Helvetica", 15, "bold"), justify=CENTER, borderwidth=0, background="#fffae6", activebackground="#fffae6", highlightthickness=3, highlightcolor="black", highlightbackground="black")
-	guess_btn.place(x=420, y=200, width=80, height=35)
+	guess_btn.place(x=420, y=195, width=80, height=35)
 	guess_btn.bind("<Enter>", lambda event: change_thickness(event, guess_btn, False))
 	guess_btn.bind("<Leave>", lambda event: change_thickness(event, guess_btn, True))
 	guess_btn.bind("<ButtonRelease-1>", lambda event: guess_click(event))
 
+	start_game()
+
 	root.mainloop()
-
-"""
-def upisivanje(x):
-	file = open("ploca.txt", "w", encoding="utf-8")
-	file.write("".join(x))
-	file.close()
-
-
-file1 = open("rjesenje.txt", "r", encoding="utf-8")
-rjesenje_str = file1.read()
-rjesenje_str = rjesenje_str.lower()
-rjesenje = []
-for i in range(len(rjesenje_str)):
-	manje_slovo = rjesenje_str[i - 1] + rjesenje_str[i]
-	if i < len(rjesenje_str) - 1:
-		vece_slovo = rjesenje_str[i] + rjesenje_str[i + 1]
-	if manje_slovo == "nj" or manje_slovo == "lj" or manje_slovo == "dž":
-		pass
-	elif vece_slovo == "lj":
-		rjesenje.append("lj")
-	elif vece_slovo == "nj":
-		rjesenje.append("nj")
-	elif vece_slovo == "dž":
-		rjesenje.append("dž")
-	else:
-		rjesenje.append(rjesenje_str[i])
-file1.close()
-
-x = []
-for i in rjesenje:
-	if i == " ":
-		x.append(i)
-	else:
-		x.append("_")
-upisivanje(x)
-
-broj_gresaka = 0
-while "_" in x and broj_gresaka != 10:
-	print("Broj grešaka {}/10".format(broj_gresaka))
-	upis = input("Unesi slovo ili pojam: ")
-	if len(upis) > 1 and upis != "lj" and upis != "nj" and upis != "dž":
-		if upis == rjesenje_str:
-			x = rjesenje
-			upisivanje(x)
-		else:
-			broj_gresaka = broj_gresaka + 1
-	else:
-		greska = True
-		for i in range(len(rjesenje)):
-			if rjesenje[i] == upis:
-				x[i] = upis
-				upisivanje(x)
-				greska = False
-
-		if greska:
-			broj_gresaka = broj_gresaka + 1
-		else:
-			print("Slovo pogođeno!")
-
-if broj_gresaka == 10:
-	print("Broj grešaka 10/10")
-	print("Izgubili ste!")
-else:
-	print("Bravo, svaka čast!")
-"""
